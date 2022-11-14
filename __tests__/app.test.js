7f41e57f83d2972b97ed1859023724518c138ee8
MARCIN <m.palenik2015@gmail.com>
2022-11-14 14:30:12 +0000

    task 3 updated

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,6 +1,10 @@
 const request = require('supertest');
 const app = require('../app');
 const db = require('../db/connection.js');
+const seed = require('../db/seeds/seed.js');
+const testData = require('../db/data/test-data');
+
+beforeEach(() => seed(testData));
 
 afterAll(() => {
   if (db.end) db.end();
@@ -27,6 +31,7 @@ describe('GET/api/categories', () => {
         .then((res) => {
           
           expect(res.body.categories).toBeInstanceOf(Array);
+          expect(res.body.categories.length).toBeGreaterThan(0);
           res.body.categories.forEach((category) => {
             expect(category).toMatchObject({
                 slug: expect.any(String),
