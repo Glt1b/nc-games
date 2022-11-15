67dd7535464392509268a8dfd047dfdfd3f1085b
MARCIN <m.palenik2015@gmail.com>
2022-11-15 10:42:24 +0000

    update2

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -37,5 +37,68 @@ describe('GET/api/categories', () => {
     });
   });
   
+  describe('GET/api/reviews', () => {
+    test('200 - responds with an array of reviews objects', () => {
+      return request(app)
+        .get('/api/reviews')
+        .expect(200)
+        .then((res) => {
+          
+          expect(res.body.reviews).toBeInstanceOf(Array);
+          expect(res.body.reviews.length).toBeGreaterThan(0);
+          res.body.reviews.forEach((review) => {
+            expect(review).toMatchObject({
+                title: expect.any(String),
+                designer: expect.any(String),
+                owner: expect.any(String),
+                review_body: expect.any(String),
+                created_at: expect.any(String),
+                votes: expect.any(Number),
+                comment_count: expect.any(Number)
+              })
+          });
+          expect(res.body.reviews).toBeSortedBy('created_at', {
+            descending: true
+          });
+        });
+    });
+  });
 
   
+describe('GET/api/review/:review_id', () => {
+  test('200 - responds with an object of review', () => {
+    return request(app)
+      .get('/api/reviews/2')
+      .expect(200)
+      .then((res) => {
+  
+        expect(res.body.review).toMatchObject({
+          title: expect.any(String),
+          designer: expect.any(String),
+          owner: expect.any(String),
+          review_body: expect.any(String),
+          created_at: expect.any(String),
+          votes: expect.any(Number)
+          })
+      });
+  });
+
+  test('400 - Bad request when pass invalid id format', () => {
+    return request(app)
+      .get('/api/reviews/lalala')
+      .expect(400)
+      .then((res) => {
+        expect(res.body.msg).toEqual('Bad Request');
+      });
+  });
+
+
+  test('404 - id not found', () => {
+    return request(app)
+      .get('/api/reviews/0')
+      .expect(404)
+      .then((res) => {
+        expect(res.body.msg).toEqual('review does not exist for id: 0');
+      });
+  });
+});
--- controllers/games.js
+++ controllers/games.js
@@ -1,8 +1,41 @@
-const { selectCategories } = require('../models/games_models.js');
+const { selectCategories, selectReviews, getReviewsVotes,
+selectReviewById } = require('../models/games_models.js');
 
 exports.getCategories = (req, res) => {
     selectCategories().then((result) => {
-      
         res.status(200).send({categories: result})
+    })
+};
+
+exports.getReviews = (req, res) => {
+    let reviews = [];
+    let count = 0;
+    selectReviews()
+    .then((result) => {
+        reviews = result;
+    }).then(() => {
+        getReviewsVotes()
+        .then((result) => {
+            
+            for ( let review of reviews ) {
+                const comment_count = result.filter((item) => item.review_id === review.review_id).length;
+                review['comment_count'] = comment_count;
+                count++;
+                if (reviews.length === count){
+                    res.status(200).send({reviews: reviews})
+                }
+            }
+            
+
+        })
+    })
+};
+
+exports.getReviewById = (req, res, next) => {
+    selectReviewById(req.params.review_id).then((result) => {
+        res.status(200).send({review: result})
+    })
+    .catch((err) => {
+        next(err);
     })
 };
